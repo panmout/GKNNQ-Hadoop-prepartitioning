07473c8133b3beadc563dd536b918625f699d60f
pmoutafis <takismoutafis@outlook.com>
2022-11-29 18:31:27 +0200

    minor update 29-11-22

--- src/main/java/gr/uth/ece/dsel/hadoop_prepartitioning/phase1/Driver1.java
+++ src/main/java/gr/uth/ece/dsel/hadoop_prepartitioning/phase1/Driver1.java
@@ -7,7 +7,6 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
--- src/main/java/gr/uth/ece/dsel/hadoop_prepartitioning/util/CreateQTreeArray.java
+++ src/main/java/gr/uth/ece/dsel/hadoop_prepartitioning/util/CreateQTreeArray.java
@@ -2,7 +2,6 @@ package gr.uth.ece.dsel.hadoop_prepartitioning.util;
 
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
